177593d18d2f977390428db0a2f7354beb4d09c7
Sevda Nur KARACA <37744354+sevdakaraca@users.noreply.github.com>
2018-07-09 14:37:23 +0300

    Some Changes in Student and Teacher register scenes

--- Assets/Scripts/TeacherRegisterScript.cs
+++ Assets/Scripts/TeacherRegisterScript.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class TeacherRegisterScript : MonoBehaviour
 {
-
+    public GameObject Email, Password, name, surname;
     public void TeacherRegisterChoice()
     {
         SceneManager.LoadScene("StudentRegisterScene");
     }
+
+
 }
