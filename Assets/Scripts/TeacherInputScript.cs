961824889c8eca50165f3d4e4e0e3a21929bdf2a
Sevda Nur KARACA <37744354+sevdakaraca@users.noreply.github.com>
2018-07-12 07:07:22 +0300

    Teacher Input

--- Assets/Scripts/TeacherInputScript.cs
+++ Assets/Scripts/TeacherInputScript.cs
@@ -5,20 +5,64 @@ using UnityEngine.SceneManagement;
 using Mono.Data.Sqlite;
 using System.Data;
 using System;
+using UnityEngine.UI;
+
 
 public class TeacherInputScript : MonoBehaviour {
+
+    public GameObject email;
+    public GameObject password;
+
+    private string Email;
+    private string Password;
 
 
     public void Input()
     {
-        //TouchScreenKeyboard.Open();
+        string conn = "URI=file:" + Application.dataPath + "/Database.db"; //Path to database.
+
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+
+        IDbCommand dbcmd = dbconn.CreateCommand();
+
+
+        string sqlQuery = "SELECT Email, Password, FROM Teacher";
+
+
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+
+
+
+        Email = email.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+
+        if (Email==Email && Password==Password)
+        {
+            print("Login sucsessful");
+        }
+        else
+        {
+
+        }
         SceneManager.LoadScene("StudentRegisterScene");
     }
+
     public void Register()
     {
         SceneManager.LoadScene("TeacherRegisterScene");
     }
-    public void testDB()
+}
+   /* public void testDB()
     {
         string conn = "URI=file:" + Application.dataPath + "/Database.db"; //Path to database.
 		
@@ -27,12 +71,13 @@ public class TeacherInputScript : MonoBehaviour {
 		dbconn.Open(); //Open connection to the database.
 
 		IDbCommand dbcmd = dbconn.CreateCommand();
-		string sqlQuery = "SELECT Email,Password FROM Teacher";
-		//string sqlQuery = "INSERT INTO Test (TestType,StuNo,q1,q2,q3,q4,q5) values ("++","++","++","++","++","++""++")";
-		//string sqlQuery2 = "SELECT StuNo, Name, Surname, Teacher FROM Student";
-		//string sqlQuery3 = "INSERT INTO Teacher (Email,Password,Name,Surname) values ("++","++","++","++")";
-		
-		dbcmd.CommandText = sqlQuery;
+
+
+        string sqlQuery = "SELECT Email, Password, FROM Teacher";
+        //string sqlQuery = "INSERT INTO Test (TestType,StuNo,q1,q2,q3,q4,q5) values ("++","++","++","++","++","++""++")";
+        //string sqlQuery3 = "INSERT INTO Teacher (Email,Password,Name,Surname) values ("++","++","++","++")";
+
+        dbcmd.CommandText = sqlQuery;
 		IDataReader reader = dbcmd.ExecuteReader();
 		
 		reader.Close();
@@ -43,5 +88,4 @@ public class TeacherInputScript : MonoBehaviour {
 		dbconn = null;
 
 
-    }
-}
+    }*/
--- Assets/Scripts/TeacherRegisterScript.cs
+++ Assets/Scripts/TeacherRegisterScript.cs
@@ -2,18 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Data;
+using System;
+using UnityEngine.UI;
+using Mono.Data.Sqlite;
+
+
 
 public class TeacherRegisterScript : MonoBehaviour
 {
-    public GameObject Email, Password, name, surname;
+    public GameObject Email, Password, Name, Surname;
     public void TeacherRegisterChoice()
     {
-       // string tempEmail = Email.GetComponent<InputField>().text;
         SceneManager.LoadScene("StudentRegisterScene");
     }
-    public void RegisterT()
+
+    public void SendDataToDB()
     {
+        string conn = "URI=file:" + Application.dataPath + "/Database/Database.db"; //Path to database.
+
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+
+        IDbCommand dbcmd = dbconn.CreateCommand();
+
+        string sqlQuery = "INSERT INTO Teacher (Email,Password,Name,Surname) values ("+Email+","+Password+","+Name+","+Surname+")";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+
+
         SceneManager.LoadScene("StudentRegisterScene");
     }
-   
+
 }
