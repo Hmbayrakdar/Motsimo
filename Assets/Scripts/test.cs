4771c5637ce8c7c08fa40b5fc25aa8d22071b23e
Sevda Nur KARACA <37744354+sevdakaraca@users.noreply.github.com>
2018-07-16 02:23:12 +0300

    .

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
 
 public class test : MonoBehaviour {
     private string[] Testnames;
